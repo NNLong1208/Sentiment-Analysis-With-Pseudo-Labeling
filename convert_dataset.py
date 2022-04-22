d864b2e33c08026867ccf8f4426763b41fdc3ae0
NNLong1208 <ngoclong1282001@gmail.com>
2022-04-22 20:31:15 +0700

    first commit

---
+++ convert_dataset.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import re
+
+def convert_train(path):
+    labels = []
+    contents = []
+    with open(path, encoding="utf-8") as f:
+        data = f.read()
+    data = re.split('train_[0-9][0-9][0-9][0-9][0-9][0-9]', data)
+    data = [d.strip() for d in data if len(d) != 0]
+    for ele in data:
+        temp = ele.split('\n')
+        temp = [t for t in temp if len(t) != 0]
+        label = int(temp[-1])
+        content = ' '.join(temp[0:len(temp)-1])
+        contents.append(content)
+        labels.append(label)
+    df = pd.DataFrame({'Content': contents,
+                       'Label': labels})
+    return df
+
+def convert_test(path):
+    contents = []
+    with open(path, encoding="utf-8") as f:
+        data = f.read()
+    data = data.split("\n\n")
+    for ele in data:
+        temp = ele.split('\n')
+        content = ' '.join(temp[1:])
+        content = content[1:len(content)-1]
+        contents.append(content)
+    df = pd.DataFrame({'Content': contents})
+    return df
+
+
+if __name__ == "__main__":
+    print("oke")
