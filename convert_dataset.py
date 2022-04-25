d9b6247af02ae52571aed8ade95ea95ad4ecbb0b
NNLong1208 <ngoclong1282001@gmail.com>
2022-04-26 00:54:06 +0700

    commit

--- convert_dataset.py
+++ convert_dataset.py
@@ -1,5 +1,7 @@
-import pandas as pd
 import re
+
+import pandas as pd
+
 
 def convert_train(path):
     labels = []
@@ -12,26 +14,26 @@ def convert_train(path):
         temp = ele.split('\n')
         temp = [t for t in temp if len(t) != 0]
         label = int(temp[-1])
-        content = ' '.join(temp[0:len(temp)-1])
+        content = ' '.join(temp[0:len(temp) - 1])
         contents.append(content)
         labels.append(label)
     df = pd.DataFrame({'Content': contents,
                        'Label': labels})
     return df
 
+
 def convert_test(path):
-    contents = []
     with open(path, encoding="utf-8") as f:
         data = f.read()
-    data = data.split("\n\n")
-    for ele in data:
-        temp = ele.split('\n')
-        content = ' '.join(temp[1:])
-        content = content[1:len(content)-1]
-        contents.append(content)
-    df = pd.DataFrame({'Content': contents})
+    data = re.split('test_[0-9][0-9][0-9][0-9][0-9][0-9]', data)
+    data = [d.strip()[1:len(d) - 1] for d in data if len(d) != 0]
+    labels = [None for _ in range(len(data))]
+    df = pd.DataFrame({'Content': data,
+                       'Label': labels})
     return df
 
 
 if __name__ == "__main__":
-    print("oke")
+    data_frame = convert_test(r"D:\pycharm\Sentiment-Analysis-With-Pseudo-Labeling\data\test.crash")
+    data_frame.to_csv("D:\pycharm\Sentiment-Analysis-With-Pseudo-Labeling\data\data_csv\data_without_label.csv",
+                      index=False)
